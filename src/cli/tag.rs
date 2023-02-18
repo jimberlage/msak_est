8c446df301c786026125509c6b198e325f6fd8da
Jim Berlage <james.berlage@gmail.com>
2023-02-18 04:57:52 -0600

    Ensure that multiple keys can be passed

--- src/cli/tag.rs
+++ src/cli/tag.rs
@@ -5,7 +5,7 @@ use jimberlage_jira_client::{IssueEditUpdate, IssueEditUpdateLabel, RestClient};
 #[derive(Debug, Args)]
 pub struct Tag {
     #[arg(long)]
-    pub jira_key: String,
+    pub jira_key: Vec<String>,
 
     #[arg(long)]
     pub jira_label: String,
@@ -23,11 +23,13 @@ pub struct Tag {
 pub fn run(args: &Tag) {
     let client = RestClient::new(&args.jira_url, &args.jira_username, &args.jira_token).unwrap();
 
-    let update = IssueEditUpdate {
-        labels: vec![IssueEditUpdateLabel::Add(args.jira_label.clone())],
-    };
+    for key in &args.jira_key {
+        let update = IssueEditUpdate {
+            labels: vec![IssueEditUpdateLabel::Add(args.jira_label.clone())],
+        };
 
-    client.edit_issue(&args.jira_url, &update).unwrap();
+        client.edit_issue(key, &update).unwrap();
+    }
 
     println!("Done!");
 }
